d4de36acea8053eb0dc0f283cf31b3ae6de2aec9
Vadim Gamzaev <Vadosbox@gmail.com>
2019-09-19 02:41:46 +0300

    Done

--- HW2.1TrafficLight/ViewController.swift
+++ HW2.1TrafficLight/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redCircle: UIView!
+    @IBOutlet weak var yellowCircle: UIView!
+    @IBOutlet weak var greenCrircle: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redCircle.alpha = 0.3
+        yellowCircle.alpha = 0.3
+        greenCrircle.alpha = 0.3
+        redCircle.layer.cornerRadius = 75
+        yellowCircle.layer.cornerRadius = 75
+        greenCrircle.layer.cornerRadius = 75
+        startButton.setTitle("Start", for: .normal)
     }
 
+    @IBAction func startButtonTap() {
+        
+        let red = redCircle.alpha
+        let yellow = yellowCircle.alpha
+        let green = greenCrircle.alpha
+        
+        
+        
+        if startButton.currentTitle == "Start" {
+            startButton.setTitle("Next", for: .normal)
+            redCircle.alpha = 1
+        }
+        else if red > 0.4 {
+            redCircle.alpha = 0.3
+            yellowCircle.alpha = 1
+        } else if yellow > 0.4 {
+            yellowCircle.alpha = 0.3
+            greenCrircle.alpha = 1
 
+        } else if green > 0.4 {
+            greenCrircle.alpha = 0.3
+            redCircle.alpha = 1
+        }
+    }
 }
 
