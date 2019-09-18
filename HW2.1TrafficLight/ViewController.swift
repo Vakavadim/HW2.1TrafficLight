4070f0acdcb4fe2d76bdb0ff19db9a3358a612dd
Vadim Gamzaev <Vadosbox@gmail.com>
2019-09-19 00:59:55 +0300

    Initial Commit

---
+++ HW2.1TrafficLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.1TrafficLight
+//
+//  Created by Вадим Гамзаев on 19/09/2019.
+//  Copyright © 2019 Вадим Гамзаев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
